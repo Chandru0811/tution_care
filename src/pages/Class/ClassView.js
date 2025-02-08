6e8661008a1cab2f26bea44348ff938396cc87a7
Ragul-dravid <dhurai737@gmail.com>
2025-02-08 19:00:01 +0530

    update tms

--- src/pages/Class/ClassView.js
+++ src/pages/Class/ClassView.js
@@ -84,25 +84,7 @@ function ClassView() {
         <div className="container-fluid px-4">
           <div className="row pb-3">
             <div className="col-md-6 col-12">
-              <div className="row mt-3  mb-2">
-                <div className="col-6 ">
-                  <p className="">Centre Name</p>
-                </div>
-                <div className="col-6">
-                  <p className="text-muted text-sm">
-                    :{" "}
-                    {centerData &&
-                      centerData.map((centerId) =>
-                        parseInt(data.centerId) === centerId.id
-                          ? centerId.centerNames || "--"
-                          : ""
-                      )}
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6 col-12">
-              <div className="row  mb-2 mt-3">
+              <div className="row mb-2">
                 <div className="col-6  ">
                   <p className="">Course</p>
                 </div>
@@ -120,8 +102,8 @@ function ClassView() {
               </div>
             </div>
             <div className="col-md-6 col-12">
-              <div className="row  mb-2">
-                <div className="col-6  ">
+              <div className="row mb-2">
+                <div className="col-6">
                   <p className="">Class Name</p>
                 </div>
                 <div className="col-6">
